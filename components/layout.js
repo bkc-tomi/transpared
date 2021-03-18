52baca8193a6db6c4fe8780da927fcfed713cd55
bkc-tomi <bkc.tomi1979@gamil.com>
2021-03-18 12:34:59 +0900

    シェア用情報追加

--- components/layout.js
+++ components/layout.js
@@ -8,8 +8,13 @@ export default function Layout({ children }) {
             <Head>
                 <title>transpared</title>
                 <link rel="icon" href="/favicon.ico" />
-                <meta property="og:title" content="My new title" key="title" />
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:site" content="@bkc30002594" />
+                <meta property="og:url" content="https://transpared.vercel.app/" />
+                <meta property="og:title" content="transpared" />
+                <meta property="og:description" content="学生のころに使っていた赤シートのように、マークダウンを使って覚えたい単語などを隠したり表示したりできるサービスです。マークダウンを知らなくても使い方とこちらが用意したエディタを使えば簡単に作れます。" />
+                <meta property="og:image" content="/icon.svg" />
             </Head>
             <Header />
             <div className="h-16"></div>
